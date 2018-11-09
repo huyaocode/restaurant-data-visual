6776e87ba42399f85ea3ce980ab460cad74707c4
huyaocode <2385249676@qq.com>
2018-11-09 22:56:09 +0800

    添加对队友的友情提示

--- src/components/ConsumeTend/index.vue
+++ src/components/ConsumeTend/index.vue
@@ -9,7 +9,7 @@
 export default {
   props: ['comments'],
   //这里的评论信息是为了你另一个图好做按照店铺分类的。你最好向我一样在public/mock中单独开一个python 程序统计好。然后我会帮你将数据传入到这个组件
-  
+  // 当然你在这里处理也可以，看你熟悉pyhton 还是 js 了
 }
 </script>
 
