b06624b37d28f9f8b3b09b0ec884cd0e7e0530e4
huyaocode <2385249676@qq.com>
2018-12-23 18:21:09 +0800

    各区域消费情况

--- src/components/CategoryStack/index.vue
+++ src/components/CategoryStack/index.vue
@@ -1,12 +1,12 @@
 <template>
-<div id="wrapper">
-  <div
-    id="categorystack"
-    ref="categorystack"
-  >
-    堆叠条形图
+  <div id="wrapper">
+    <div
+      id="categorystack"
+      ref="categorystack"
+    >
+      堆叠条形图
+    </div>
   </div>
-</div>
 
 </template>
 
@@ -17,7 +17,7 @@ export default {
   mounted () {
     var mychart = echarts.init(this.$refs.categorystack);
 
-    var option = {
+    let option = {
       tooltip: {
         trigger: 'axis',
         axisPointer: {            // 坐标轴指示器，坐标轴触发有效
@@ -25,7 +25,9 @@ export default {
         }
       },
       legend: {
-        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎'],
+        // 餐厅种类
+        data: ['川菜', '火锅', '烧烤', '自助餐', '快餐简餐', '面包甜点', '西餐', '韩国料理', '日本料理',
+          '小吃面食', '咖啡厅', '东南亚菜', '粤菜', '江浙菜', '酒吧', '其他'],
         textStyle:{
           color: '#fff'
         }
@@ -34,13 +36,20 @@ export default {
         left: '3%',
         right: '4%',
         bottom: '3%',
-        containLabel: true,
-        color: '#fff'
+        containLabel: true
       },
       xAxis: {
-        type: 'value'
+        type: 'value',
+        axisLine: {
+          lineStyle: {
+            color:'#fff'
+          }
+        },
       },
       yAxis: {
+        type: 'category',
+        // 这里放地区种类
+        data: ['人民公园', '圆艺山', '六里村', '安县', '江油市中心城区', '涪城万达广场', '高水', '南河', '高新区', '三台县中心城区', '凯德广场', '芙蓉汉城', '御营坝', '西科大'],
         type: 'category',
         nameTextStyle: {
           color:'#fff',
@@ -51,11 +60,10 @@ export default {
             color:'#fff'
           }
         },
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
       },
       series: [
         {
-          name: '直接访问',
+          name: '川菜',
           type: 'bar',
           stack: '总量',
           label: {
@@ -64,10 +72,10 @@ export default {
               position: 'insideRight'
             }
           },
-          data: [320, 302, 301, 334, 390, 330, 320]
+          data: [6, 19, 10, 0, 18, 27, 30, 31, 11, 18, 12, 21, 21, 13]
         },
         {
-          name: '邮件营销',
+          name: '火锅',
           type: 'bar',
           stack: '总量',
           label: {
@@ -76,10 +84,10 @@ export default {
               position: 'insideRight'
             }
           },
-          data: [120, 132, 101, 134, 90, 230, 210]
+          data: [8, 23, 19, 0, 61, 31, 42, 40, 15, 46, 12, 15, 38, 10]
         },
         {
-          name: '联盟广告',
+          name: '烧烤',
           type: 'bar',
           stack: '总量',
           label: {
@@ -88,10 +96,10 @@ export default {
               position: 'insideRight'
             }
           },
-          data: [220, 182, 191, 234, 290, 330, 310]
+          data: [0, 2, 1, 0, 14, 5, 8, 7, 2, 8, 5, 6, 4, 3]
         },
         {
-          name: '视频广告',
+          name: '自助餐',
           type: 'bar',
           stack: '总量',
           label: {
@@ -100,10 +108,10 @@ export default {
               position: 'insideRight'
             }
           },
-          data: [150, 212, 201, 154, 190, 330, 410]
+          data: [2, 1, 2, 0, 2, 5, 3, 0, 1, 5, 2, 2, 1, 3]
         },
         {
-          name: '搜索引擎',
+          name: '快餐简餐',
           type: 'bar',
           stack: '总量',
           label: {
@@ -112,8 +120,140 @@ export default {
               position: 'insideRight'
             }
           },
-          data: [820, 832, 901, 934, 1290, 1330, 1320]
-        }
+          data: [17, 4, 12, 0, 12, 18, 14, 17, 7, 18, 5, 2, 11, 18]
+        },
+        {
+          name: '面包甜点',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [17, 6, 8, 0, 34, 29, 11, 14, 1, 26, 32, 1, 9, 8]
+        },
+        {
+          name: '西餐',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [6, 3, 0, 0, 4, 3, 1, 0, 0, 5, 2, 1, 1, 2]
+        },
+        {
+          name: '韩国料理',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [3, 0, 0, 0, 0, 3, 1, 4, 1, 0, 4, 0, 0, 2]
+        },
+        {
+          name: '日本料理',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [0, 1, 0, 0, 1, 1, 0, 1, 0, 5, 2, 0, 0, 1]
+        },
+        {
+          name: '小吃面食',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [11, 3, 6, 0, 17, 24, 15, 16, 7, 10, 17, 0, 8, 14]
+        },
+        {
+          name: '咖啡厅',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [14, 1, 2, 0, 7, 7, 0, 7, 1, 3, 2, 4, 3, 2]
+        },
+        {
+          name: '东南亚菜',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+        },
+        {
+          name: '粤菜',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [2, 0, 0, 0, 1, 2, 1, 1, 0, 1, 3, 0, 0, 0]
+        },
+        {
+          name: '江浙菜',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1]
+        },
+        {
+          name: '酒吧',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [4, 1, 0, 0, 3, 2, 1, 4, 0, 2, 3, 3, 1, 1]
+        },
+        {
+          name: '其他',
+          type: 'bar',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'insideRight'
+            }
+          },
+          data: [4, 8, 3, 0, 21, 18, 8, 7, 6, 7, 9, 5, 9, 2]
+        },
       ]
     };
     mychart.setOption(option);
@@ -125,10 +265,10 @@ export default {
 
 <style scoped>
 #wrapper {
-   height: 700px;
-   overflow-y: scroll;
-   /* background-color: #333; */
-   color: #fff;
+  height: 700px;
+  overflow-y: scroll;
+  /* background-color: #333; */
+  color: #fff;
 }
 #categorystack {
   height: 1200px;
